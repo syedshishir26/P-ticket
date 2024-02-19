0a9b334549aafbdcb76e5ac802ad7a36d9f80f69
syedshishir26 <syedshishir26@gmail.com>
2024-02-19 13:49:22 +0600

    almost done

--- scripts.js
+++ scripts.js
@@ -1,45 +1,90 @@
+let arr = []
 const seat = document.getElementsByClassName('seat');
 for (let i of seat) {
-    i.addEventListener('click', function () {
 
-        let totalPrice = parseInt(document.getElementById('totalPrice').innerText);
-        if (totalPrice < 2200) {
-            // change bg and text color
-            i.classList.add('bg-[#1DD100]');
-            i.classList.add('text-white');
-            i.classList.remove('bg-opacity-10');
+    i.addEventListener('click', function () {
+        if (!arr.includes(i.innerText)) {
+            let totalPrice = parseInt(document.getElementById('totalPrice').innerText);
+            if (totalPrice < 2200) {
+                arr.push(i.innerText);
+                // change bg and text color
+                i.classList.add('bg-[#1DD100]');
+                i.classList.add('text-white');
+                i.classList.remove('bg-opacity-10');
+
+                // change rest seat numbers
+                let seatLefts = parseInt(document.getElementById('seatLefts').innerText);
+                let seatLeftAfterSelect = seatLefts - 1;
+                document.getElementById('seatLefts').innerText = seatLeftAfterSelect;
+                // change booked seat numbers
+                let seatBooked = parseInt(document.getElementById('seatBooked').innerText);
+                let seatBookedAfterSelect = seatBooked + 1;
+                document.getElementById('seatBooked').innerText = seatBookedAfterSelect;
+                // creating booked seat info
+                let appentElement = document.getElementById('newSeats');
+
+                let seatName = document.createElement('p');
+                seatName.classList.add('seatName', (i.innerText).toLowerCase())
+                seatName.innerText = i.innerText;
+                let className = document.createElement('p');
+                className.classList.add('seatClass',(i.innerText).toLowerCase())
+                className.innerText = 'Economy';
+                let pricePerSeat = document.createElement('p');
+                pricePerSeat.classList.add('seatPrice',(i.innerText).toLowerCase())
+                pricePerSeat.innerText = 550;
+
+                // appending new booked seat
+                appentElement.appendChild(seatName);
+                appentElement.appendChild(className);
+                appentElement.appendChild(pricePerSeat);
+                // changing total price
+                let newPrice = totalPrice + 550;
+                document.getElementById('totalPrice').innerText = newPrice;
+                // document.getElementById('grandTotal').innerText = newPrice;
+
+                // eneble next button
+                const nextBtn = document.getElementById('nextButton');
+                nextBtn.classList.remove('bg-opacity-20');
+                nextBtn.removeAttribute('disabled');
+            
+            }
+
+
+
+        }
+        else {
+            i.classList.remove('bg-[#1DD100]');
+            i.classList.remove('text-white');
+            i.classList.add('bg-opacity-10');
+
+            
+            arr.splice(arr.indexOf(i.innerText),1)
 
             // change rest seat numbers
             let seatLefts = parseInt(document.getElementById('seatLefts').innerText);
-            let seatLeftAfterSelect = seatLefts - 1;
+            let seatLeftAfterSelect = seatLefts + 1;
             document.getElementById('seatLefts').innerText = seatLeftAfterSelect;
+
             // change booked seat numbers
             let seatBooked = parseInt(document.getElementById('seatBooked').innerText);
-            let seatBookedAfterSelect = seatBooked + 1;
+            let seatBookedAfterSelect = seatBooked - 1;
             document.getElementById('seatBooked').innerText = seatBookedAfterSelect;
-            // creating booked seat info
-            let appentElement = document.getElementById('newSeats');
-            let seatName = document.createElement('p');
-            seatName.innerText = i.innerText;
-            let className = document.createElement('p');
-            className.innerText = 'Economy';
-            let pricePerSeat = document.createElement('p');
-            pricePerSeat.innerText = 550;
-            // appending new booked seat
-            appentElement.appendChild(seatName);
-            appentElement.appendChild(className);
-            appentElement.appendChild(pricePerSeat);
-            // changing total price
-            let newPrice = totalPrice + 550;
-            document.getElementById('totalPrice').innerText = newPrice;
-            document.getElementById('grandTotal').innerText = newPrice;
 
-            // eneble next button
-            const nextBtn = document.getElementById('nextButton');
-            nextBtn.classList.remove('bg-opacity-20');
-            nextBtn.removeAttribute('disabled');
+            // changing total price
+            document.getElementById('totalPrice').innerText = parseInt(document.getElementById('totalPrice').innerText)-550;
+            
+            
+            
+            // pop seat
+            let newSeat =document.getElementsByClassName((i.innerText).toLowerCase());
+            for(let j of newSeat){
+                j.classList.add('hidden')
+            }
         }
-    })
+        document.getElementById('grandTotal').innerText = parseInt(document.getElementById('totalPrice').innerText)
+        
+    }
+    )
 }
 // check cupon
 
@@ -63,10 +108,3 @@ document.getElementById('cupon').addEventListener('click', function () {
         document.getElementById('cuponCode').value = '';
     }
 })
-// enable next button
-// document.getElementById('nextButton').addEventListener('click', function () {
-//     if (document.getElementById('phNumber').value !== isNaN) {
-
-//     }
-
-// })
